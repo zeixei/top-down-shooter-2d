1cbabf93cb7f9341571ed98de6aecdce6dd0a5e4
zei <zzeynepsudegenc@gmail.com>
2023-05-29 15:51:26 +0300

    shooting game

---
+++ Assets/Scripts/Game/Player/Bullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        DestroyWhenOffScreen();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<EnemyMovement>())
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    private void DestroyWhenOffScreen()
+    {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+
+        if (screenPosition.x < 0 ||
+            screenPosition.x > _camera.pixelWidth ||
+            screenPosition.y < 0 ||
+            screenPosition.y > _camera.pixelHeight)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
